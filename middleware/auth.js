8d6a3fd5f756372473b419f31e2f3461166ca2e1
CHISOM EDOKA <edokachisom48@gmail.com>
2023-10-31 02:31:15 +0100

    set up authentication

---
+++ middleware/auth.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function auth(req, res, next) {
+  const token = req.header("Authorization");
+  if (!token) return res.status(401).send("Access denied, No token provided");
+
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = decoded;
+    next();
+  } catch (ex) {
+    res.status(400).send("Invalid token.");
+  }
+};
